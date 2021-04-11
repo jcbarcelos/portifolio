bed4449175926d0f752cb380503d44cf5d5739f3
jcbarcelos <jcbarcelos@email.com>
2021-04-10 21:31:41 -0300

    lista de usuarios

---
+++ react/mobile/rncrud/src/data/user.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'Jão Silva',
+        email: "josil@email.com",
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Julio Cesar',
+        email: "julio@email.com",
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'
+    },
+    {
+        id: 3,
+        name: 'Rafael maria',
+        email: "rafael@email.com",
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Mariana',
+        email: "maria@email.com",
+        avatarUrl: 'https://cdn.pixabay.com/photo/2016/08/31/11/54/user-1633249_960_720.png'
+    }
+]
--- react/mobile/rncrud/src/views/UserList.js
+++ react/mobile/rncrud/src/views/UserList.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import {Text } from 'react-native';
+import {Text, View, FlatList} from 'react-native';
+import users from '../data/user';
+import {ListItem, Avatar} from 'react-native-elements';
 
 export default props => {
+  function fetUserItem({item: user}) {
     return (
-        <Text>ListForm</Text>
-    )
-}
+      <ListItem
+        key={user.id}
+        bottomDivider
+        onPress={() => props.navigation.navigate('UserFom')}>
+        <Avatar source={{uri: user.avatarUrl}} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+        <ListItem.Chevron color="red" size={25} />
+      </ListItem>
+    );
+  }
+  return (
+    <View>
+      <FlatList
+        keyExtractor={user => user.id.toString()}
+        data={users}
+        renderItem={fetUserItem}
+      />
+    </View>
+  );
+};
